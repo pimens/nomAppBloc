a968308a522b6d0f35622f7d798f064017ea28b8
pimen <imankidis@gmail.com>
2020-05-26 07:02:56 +0700

    update ui recent

--- lib/src/ui/Recent.dart
+++ lib/src/ui/Recent.dart
@@ -1,3 +1,4 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:KimochiApps/src/ui/DetailOrder.dart';
 import 'package:KimochiApps/src/ui/component/Legend.dart';
@@ -27,9 +28,29 @@ class _RecentState extends State<Recent> {
     // data_login = prefs.getString('login') ?? '';
   }
 
+  Widget judul(BuildContext context, String alamat) {
+    return ExpandablePanel(
+      // header: Text("xxx"),
+      collapsed: Text(
+        alamat,
+        style: TextStyle(fontSize: 25.0, fontFamily: 'ZCOOL QingKe HuangYou'),
+        softWrap: true,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+      ),
+      expanded: Text(
+        alamat,
+        softWrap: true,
+        style: TextStyle(fontSize: 25.0, fontFamily: 'ZCOOL QingKe HuangYou'),
+      ),
+      tapHeaderToExpand: true,
+      hasIcon: true,
+    );
+  }
+
   Future ambildata() async {
     http.Response hasil = await http.get(
-        Uri.encodeFull(Constants.server+"Api/getTrx/" + hp),
+        Uri.encodeFull(Constants.server + "Api/getTrx/" + hp),
         headers: {"Accept": "application/json"});
     if (!mounted) return;
     this.setState(() {
@@ -48,12 +69,12 @@ class _RecentState extends State<Recent> {
       });
     }
   }
+
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
     timer.cancel();
   }
-  
 
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -94,104 +115,134 @@ class _RecentState extends State<Recent> {
                 itemCount: current == null ? 0 : current.length,
                 itemBuilder: (BuildContext context, int index) {
                   // Map menu = dataMenu[index];
-                  return Column(
-                    children: <Widget>[
-                      FlatButton(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              margin: EdgeInsets.only(bottom: 8),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.calendar_today,
-                                    color: Color.fromRGBO(243, 156, 18, 20),
-                                    size: 25,
-                                  ),
-                                  Text(
-                                    " " +
-                                        current[index]['tanggal'].toString() +
-                                        "  ",
-                                    style: TextStyle(
-                                      fontFamily: 'ZCOOL QingKe HuangYou',
-                                      color: Colors.black,
-                                      fontSize: 25,
-                                    ),
-                                    textAlign: TextAlign.left,
-                                  ),
-                                  Icon(
-                                    Icons.local_atm,
-                                    color: Color.fromRGBO(243, 156, 18, 20),
-                                    size: 25,
-                                  ),
-                                  Text(
-                                    " " +
-                                        current[index]['total'].toString() +
-                                        "  ",
-                                    style: TextStyle(
-                                      fontFamily: 'ZCOOL QingKe HuangYou',
-                                      color: Colors.black,
-                                      fontSize: 25,
-                                    ),
-                                    textAlign: TextAlign.left,
-                                  ),
-                                  Icon(
-                                    current[index]['status'].toString() == "0"
-                                        ? Icons.close
-                                        : current[index]['status'].toString() ==
-                                                "1"
-                                            ? Icons.check_box
-                                            : current[index]['status']
+                  return Container(
+                      // height: MediaQuery.of(context).size.height / 6,
+                      // width: MediaQuery.of(context).size.width / 1.3,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Color.fromRGBO(243, 156, 18, 20),
+                                width: 0,
+                                style: BorderStyle.solid),
+                            borderRadius: BorderRadius.circular(10.0)),
+                        elevation: 3.0,
+                        child: Container(
+                          padding: EdgeInsets.only(top: 15),
+                          child: Column(
+                            children: <Widget>[
+                              FlatButton(
+                                child: Column(
+                                  children: <Widget>[
+                                    Container(
+                                      margin: EdgeInsets.only(bottom: 8),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.calendar_today,
+                                            color: Color.fromRGBO(
+                                                243, 156, 18, 20),
+                                            size: 25,
+                                          ),
+                                          Text(
+                                            " " +
+                                                current[index]['tanggal']
+                                                    .toString() +
+                                                "  ",
+                                            style: TextStyle(
+                                              fontFamily:
+                                                  'ZCOOL QingKe HuangYou',
+                                              color: Colors.black,
+                                              fontSize: 25,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                          Icon(
+                                            Icons.local_atm,
+                                            color: Color.fromRGBO(
+                                                243, 156, 18, 20),
+                                            size: 25,
+                                          ),
+                                          Text(
+                                            " " +
+                                                current[index]['total']
+                                                    .toString() +
+                                                "  ",
+                                            style: TextStyle(
+                                              fontFamily:
+                                                  'ZCOOL QingKe HuangYou',
+                                              color: Colors.black,
+                                              fontSize: 25,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                          Icon(
+                                            current[index]['status']
                                                         .toString() ==
-                                                    "2"
-                                                ? Icons.playlist_add_check
-                                                : Icons.motorcycle,
-                                    color: Color.fromRGBO(243, 156, 18, 20),
-                                    size: 25.0,
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.location_on,
-                                  color: Color.fromRGBO(243, 156, 18, 20),
-                                  size: 25,
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    current[index]['alamat'].toString(),
-                                    style: TextStyle(
-                                      fontFamily: 'ZCOOL QingKe HuangYou',
-                                      color: Colors.black,
-                                      fontSize: 25,
+                                                    "0"
+                                                ? Icons.close
+                                                : current[index]['status']
+                                                            .toString() ==
+                                                        "1"
+                                                    ? Icons.check_box
+                                                    : current[index]
+                                                                    ['status']
+                                                                .toString() ==
+                                                            "2"
+                                                        ? Icons
+                                                            .playlist_add_check
+                                                        : Icons.motorcycle,
+                                            color: Color.fromRGBO(
+                                                243, 156, 18, 20),
+                                            size: 25.0,
+                                          ),
+                                        ],
+                                      ),
                                     ),
-                                    textAlign: TextAlign.left,
-                                  ),
+                                    Row(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.location_on,
+                                          color:
+                                              Color.fromRGBO(243, 156, 18, 20),
+                                          size: 25,
+                                        ),
+                                        Expanded(
+                                          child: Text(
+                                            current[index]['alamat'].toString(),
+                                            style: TextStyle(
+                                              fontFamily:
+                                                  'ZCOOL QingKe HuangYou',
+                                              color: Colors.black,
+                                              fontSize: 25,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ],
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => DetailOrder(
+                                          notrx: current[index]['notrx']
+                                              .toString()),
+                                    ),
+                                  );
+                                },
+                              ),
+                              Divider(
+                                thickness: 2,
+                                color: Color.fromRGBO(243, 156, 18, 10),
+                              ),
+                            ],
+                          ),
                         ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => DetailOrder(
-                                  notrx: current[index]['notrx'].toString()),
-                            ),
-                          );
-                        },
-                      ),
-                      Divider(
-                        thickness: 2,
-                        color: Color.fromRGBO(243, 156, 18, 10),
-                      ),
-                    ],
-                  );
+                      ));
                 },
               ),
             ),
